8fe0f443008a70fe2ef3edb0ad6bbbe5f0515940
SzymonKop <szymon.kopcinski@wp.pl>
2025-02-02 13:56:14 +0100

    Pierwszy program

--- 2projekt/Program.cs
+++ 2projekt/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("aplikacja do mnożenia liczby przez 10 \npodaj liczbe: ");
+var tekstZKonsoli = Console.ReadLine();
+var czyUdaloSiePrzekonwertowacNaLiczbe = int.TryParse(tekstZKonsoli, out int result);
+if (czyUdaloSiePrzekonwertowacNaLiczbe)
+{
+    Console.WriteLine("wynik to:" + (result * 10));
+}
+else
+{
+    Console.WriteLine($"podany tekst [{tekstZKonsoli}] nie da się przekonwertować na liczbe");
+}
