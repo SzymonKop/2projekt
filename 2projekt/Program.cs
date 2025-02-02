1594f6b1f4e2db91414e37c062c1baecd1261392
SzymonKop <szymon.kopcinski@wp.pl>
2025-02-02 14:28:12 +0100

    projekt2zmiana

--- 2projekt/Program.cs
+++ 2projekt/Program.cs
@@ -1,11 +1,31 @@
-﻿Console.WriteLine("aplikacja do mnożenia liczby przez 10 \npodaj liczbe: ");
-var tekstZKonsoli = Console.ReadLine();
-var czyUdaloSiePrzekonwertowacNaLiczbe = int.TryParse(tekstZKonsoli, out int result);
-if (czyUdaloSiePrzekonwertowacNaLiczbe)
+﻿internal class Program
 {
-    Console.WriteLine("wynik to:" + (result * 10));
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("aplikacja do dodawania\npodaj liczbe pierwsza: ");
+        var liczbaPierwsza = Console.ReadLine();
+        Console.WriteLine("podaj liczbe druga");
+        var liczbaDruga = Console.ReadLine();
+
+        var czyUdaloSiePrzekonwertowac1NaLiczbe = int.TryParse(liczbaPierwsza, out int liczbaPierwszaPoKonwersji);
+        var czyUdaloSiePrzekonwertowac2NaLiczbe = int.TryParse(liczbaPierwsza, out int liczbaDrugaPoKonwersji);
+        if (czyUdaloSiePrzekonwertowac1NaLiczbe && czyUdaloSiePrzekonwertowac2NaLiczbe)
+        {
+            var wynik = Calculator.Add(liczbaPierwszaPoKonwersji, liczbaDrugaPoKonwersji);
+            Console.WriteLine("wynik to:" + wynik);
+        }
+        else
+        {
+            Console.WriteLine($"jeden z podanych argumentów nie był liczbą");
+        }
+    }
 }
-else
+
+public static class Calculator
 {
-    Console.WriteLine($"podany tekst [{tekstZKonsoli}] nie da się przekonwertować na liczbe");
+
+    public static int Add(int x, int y)
+    {
+        return x + y;
+    }
 }
