b472866b661f1991511d95763b8185a638345342
SzymonKop <szymon.kopcinski@wp.pl>
2025-02-04 22:45:59 +0100

    Odejmowanie aktu.

--- 2projekt/Program.cs
+++ 2projekt/Program.cs
@@ -4,29 +4,51 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("aplikacja do dodawania\npodaj liczbe pierwsza: ");
-        var liczbaPierwsza = Console.ReadLine();
-        Console.WriteLine("podaj liczbe druga");
-        var liczbaDruga = Console.ReadLine();
 
-        var czyUdaloSiePrzekonwertowac1NaLiczbe = int.TryParse(liczbaPierwsza, out int liczbaPierwszaPoKonwersji);
-        var czyUdaloSiePrzekonwertowac2NaLiczbe = int.TryParse(liczbaDruga, out int liczbaDrugaPoKonwersji);
-        if (czyUdaloSiePrzekonwertowac1NaLiczbe && czyUdaloSiePrzekonwertowac2NaLiczbe)
+        do
         {
-            var wynik = Calculator.Add(liczbaPierwszaPoKonwersji, liczbaDrugaPoKonwersji);
-            Console.WriteLine("wynik to:" + wynik);
+            Console.WriteLine("aplikacja do dodawania\npodaj liczbe pierwsza: ");
+            var liczbaPierwsza = Console.ReadLine();
+            Console.WriteLine("podaj operacje arytmetyczna");
+            var operacjaArytmetyczna = Console.ReadLine();
+            Console.WriteLine("podaj liczbe druga");
+            var liczbaDruga = Console.ReadLine();
+
+            var czyUdaloSiePrzekonwertowac1NaLiczbe = Int64.TryParse(liczbaPierwsza, out Int64 liczbaPierwszaPoKonwersji);
+            var czyUdaloSiePrzekonwertowac2NaLiczbe = Int64.TryParse(liczbaDruga, out Int64 liczbaDrugaPoKonwersji);
+            if (czyUdaloSiePrzekonwertowac1NaLiczbe && czyUdaloSiePrzekonwertowac2NaLiczbe)
+            {
+                if (operacjaArytmetyczna == "+")
+                {
+                    var wynik = Calculator.Add(liczbaPierwszaPoKonwersji, liczbaDrugaPoKonwersji);
+                    Console.WriteLine($"wynik to: {wynik}");
+                }
+                else if (operacjaArytmetyczna == "-")
+                {
+                    var wynik = Calculator.Minus(liczbaPierwszaPoKonwersji, liczbaDrugaPoKonwersji);
+                    Console.WriteLine("wynik to:" + wynik);
+                }
+                else
+                {
+                    Console.WriteLine("error - operacja arytmetyczna nie obsługiwana");
+                }
+            }
+            else
+            {
+                Console.WriteLine("jeden z podanych argumentów nie był liczbą");
+            }
         }
-        else
-        {
-            Console.WriteLine($"jeden z podanych argumentów nie był liczbą");
-        }
+        while (true);
     }
 }
 
 public static class Calculator
 {
-
-    public static int Add(int x, int y)
+    public static Int64 Minus(Int64 x, Int64 y)
+    {
+        return x - y;
+    }
+    public static Int64 Add(Int64 x, Int64 y)
     {
         return x + y;
     }
