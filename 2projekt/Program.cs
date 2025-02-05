897a6ac7f9955740b80bfda28e7e3a4861bf6e83
SzymonKop <szymon.kopcinski@wp.pl>
2025-02-05 22:01:53 +0100

    uproszczony kalkulator

--- 2projekt/Program.cs
+++ 2projekt/Program.cs
@@ -4,48 +4,54 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-
+        Console.WriteLine("aplikacja arytmetyczna");
         do
         {
-            Console.WriteLine("aplikacja do dodawania\npodaj liczbe pierwsza: ");
-            var liczbaPierwsza = Console.ReadLine();
-            Console.WriteLine("podaj operacje arytmetyczna");
-            var operacjaArytmetyczna = Console.ReadLine();
-            Console.WriteLine("podaj liczbe druga");
-            var liczbaDruga = Console.ReadLine();
-
-            var czyUdaloSiePrzekonwertowac1NaLiczbe = Int64.TryParse(liczbaPierwsza, out Int64 liczbaPierwszaPoKonwersji);
-            var czyUdaloSiePrzekonwertowac2NaLiczbe = Int64.TryParse(liczbaDruga, out Int64 liczbaDrugaPoKonwersji);
-            if (czyUdaloSiePrzekonwertowac1NaLiczbe && czyUdaloSiePrzekonwertowac2NaLiczbe)
+            try
             {
-                if (operacjaArytmetyczna == "+")
+                Console.WriteLine("\npodaj liczbe pierwsza:");
+                var liczbaPierwsza = Console.ReadLine();
+                Console.WriteLine("podaj operacje arytmetyczna");
+                var operacjaArytmetyczna = Console.ReadLine();
+                Console.WriteLine("podaj liczbe druga");
+                var liczbaDruga = Console.ReadLine();
+
+                var czyUdaloSiePrzekonwertowac1NaLiczbe = Int64.TryParse(liczbaPierwsza, out Int64 liczbaPierwszaPoKonwersji);
+                var czyUdaloSiePrzekonwertowac2NaLiczbe = Int64.TryParse(liczbaDruga, out Int64 liczbaDrugaPoKonwersji);
+                if (czyUdaloSiePrzekonwertowac1NaLiczbe && czyUdaloSiePrzekonwertowac2NaLiczbe)
                 {
-                    var wynik = Calculator.Add(liczbaPierwszaPoKonwersji, liczbaDrugaPoKonwersji);
-                    Console.WriteLine($"wynik to: {wynik}");
-                }
-                else if (operacjaArytmetyczna == "-")
-                {
-                    var wynik = Calculator.Minus(liczbaPierwszaPoKonwersji, liczbaDrugaPoKonwersji);
-                    Console.WriteLine("wynik to:" + wynik);
-                }
-                else if (operacjaArytmetyczna == "*")
-                {
-                    var wynik = Calculator.Multiply(liczbaPierwszaPoKonwersji, liczbaDrugaPoKonwersji);
-                    Console.WriteLine("wynik to:" + wynik);
-                }
-                else if (operacjaArytmetyczna == "/")
-                { 
-                    var wynik = Calculator.Divide(liczbaPierwszaPoKonwersji, liczbaDrugaPoKonwersji);
-                Console.WriteLine("wynik to:" + wynik);
+                    Int64 wynik;
+                    switch (operacjaArytmetyczna)
+                    {
+                        case "+":
+                            wynik = Calculator.Add(liczbaPierwszaPoKonwersji, liczbaDrugaPoKonwersji);
+                            Console.WriteLine($"wynik to: {wynik}");
+                            break;
+                        case "-":
+                            wynik = Calculator.Minus(liczbaPierwszaPoKonwersji, liczbaDrugaPoKonwersji);
+                            Console.WriteLine("wynik to:" + wynik);
+                            break;
+                        case "*":
+                            wynik = Calculator.Multiply(liczbaPierwszaPoKonwersji, liczbaDrugaPoKonwersji);
+                            Console.WriteLine("wynik to:" + wynik);
+                            break;
+                        case "/":
+                            wynik = Calculator.Divide(liczbaPierwszaPoKonwersji, liczbaDrugaPoKonwersji);
+                            Console.WriteLine("wynik to:" + wynik);
+                            break;
+                        default:
+                            Console.WriteLine("error - operacja arytmetyczna nie obsługiwana");
+                            break;
+                    }
                 }
                 else
                 {
-                    Console.WriteLine("error - operacja arytmetyczna nie obsługiwana");
+                    Console.WriteLine("jeden z podanych argumentów nie był liczbą");
                 }
             }
-            else
+            catch (Exception ex)
             {
-                Console.WriteLine("jeden z podanych argumentów nie był liczbą");
+                Console.WriteLine($"Error: {ex.Message}");
             }
         }
         while (true);
@@ -56,7 +62,8 @@ public static class Calculator
 
 {
     public static Int64 Multiply(Int64 x, Int64 y)
-        { return x * y; 
+    {
+        return x * y;
     }
     public static Int64 Divide(Int64 x, Int64 y)
     {
